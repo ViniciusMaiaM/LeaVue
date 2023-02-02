52fa78f0bb4d994d0a11775f35a029dee18a857e
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-02 10:40:47 -0300

    chore: add component data

--- 04-components/src/components/FirstComponent.vue
+++ 04-components/src/components/FirstComponent.vue
@@ -1,9 +1,19 @@
 <template>
-    <h1>Hello Vue!</h1>
+    <!-- Os elementos são colocados em uma div para que se possa enviar mais de um elemento -->
+    <div>
+        <h1>Hello Vue!</h1>
+        <p>My name is {{ name }}, work as a {{ profission }}</p>
+    </div>
 </template>
 
 <script>
     export default{
-        name: 'FirstComponent'
+        name: 'FirstComponent',
+        data() {
+            return{
+                name: "Vinicius",
+                profission: "Developer"
+            }
+        }
     }
 </script>
