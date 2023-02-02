142a5168baf098982b34855f0541a91ae9875f49
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-02 10:29:32 -0300

    feat: add first component

---
+++ 04-components/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<FirstComponent />
+	<!-- Chamada de primeiro componente, ó exibindo no html -->
+</template>
+<script>
+
+	import FirstComponent from './components/FirstComponent.vue'; 
+	export default{
+		name:'App',
+		components: { //Determinando quais componentes vão estar na página
+			FirstComponent
+		}
+	}
+</script>
