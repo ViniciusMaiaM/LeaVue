9020194695d49806a679d02b39e32beabf161b58
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-02 11:38:01 -0300

    feat: add initial use of lifecycle #07

--- 04-components/src/App.vue
+++ 04-components/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-	<FirstComponent />
-	<!-- Chamada de primeiro componente, ó exibindo no html -->
+	<div>
+		<FirstComponent />
+		<!-- Chamada de primeiro componente, ó exibindo no html -->
+		<LifeCycle />
+	</div>
 </template>
 <script>
 
 	import FirstComponent from './components/FirstComponent.vue'; 
+	import LifeCycle from './components/LifeCycle.vue';
 	export default{
 		name:'App',
 		components: { //Determinando quais componentes vão estar na página
-			FirstComponent
+			FirstComponent,
+			LifeCycle
 		}
 	}
 </script>
---
+++ 04-components/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>My name is: {{ name }}</h1>
+</template>
+
+<script>
+    export default{
+        name: 'LifeCycle',
+        data() {
+            return{
+                name: 'Still do not know'
+            }
+        },
+        created(){ 
+            setTimeout(() => { //Troca para esse nome após o tempo de timeout
+                this.name = 'Vinicius'
+            }, 1000);
+        },
+        mounted() {
+            setTimeout(() =>{
+                this.name = 'Test'
+            }, 2000);
+        }
+    }
+</script>
