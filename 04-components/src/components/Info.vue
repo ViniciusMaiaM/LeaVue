5adea7140192f1016c54dbc37b8f3ed2a5302fba
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-03 19:11:19 -0300

    add directives

--- 04-components/src/components/Info.vue
+++ 04-components/src/components/Info.vue
@@ -1,17 +1,27 @@
 <template>
     <div>
-        <p>I'm a student at the moment.</p>
+        <p v-if="working">I'm working at the moment.</p>
+        <p v-else>Searching for a job.</p>
         <p>I use the following technologies:</p>
         <ul>
             <li>JavaScript</li>
             <li>TypeScript</li>
             <li>Python</li>
         </ul>
+        <p v-show="show_email">Send a message to: {{ email }}</p>
     </div>
 </template>
 
 <script>
     export default{
-        name: 'Info'
+        name: 'Info',
+
+        data(){
+            return{
+                working: false,
+                show_email: true,
+                email: 'viniciusmaiamarinho1@gmail.com'
+            }
+        }
     }
 </script>
