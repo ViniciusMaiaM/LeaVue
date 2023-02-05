d3e8517e00dbbf2869596c2076bfe00b2c73a762
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-05 12:22:39 -0300

    Add picture

--- 04-components/src/components/Info.vue
+++ 04-components/src/components/Info.vue
@@ -9,18 +9,26 @@
             <li>Python</li>
         </ul>
         <p v-show="show_email">Send a message to: {{ email }}</p>
+        <p>To acess my portfolio <a v-bind:href="meu_link">click here</a></p>
+        <!-- v-bind é utilizada para fazer ligações e dar valores dinamicos as tags html -->
+        <Picture />
     </div>
 </template>
 
 <script>
+    import Picture from './Picture.vue';
     export default{
         name: 'Info',
-
+        components: {
+            Picture
+        },
         data(){
             return{
                 working: false,
                 show_email: true,
-                email: 'viniciusmaiamarinho1@gmail.com'
+                email: 'viniciusmaiamarinho1@gmail.com',
+                meu_link: 'https://google.com'
+                //v-bind faz ligação entre um dado dinâmico e o que está no template
             }
         }
     }
---
+++ 04-components/src/components/Picture.vue
@@ -0,0 +1,17 @@
+<template>
+    <img :src="avatar" :alt="description">
+    <!-- Os dois pontos funcionam da mesma maneira que colocar o  v-bind -->
+</template>
+
+<script>
+export default{
+    name: 'Picture',
+
+    data(){
+        return{
+            avatar: '/img/avatar.png',
+            description: 'Vinicius Maia Avatar'
+        }
+    }
+}
+</script>
