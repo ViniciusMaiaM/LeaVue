d16f429787177dc05e4fd02206cc681ead0a2853
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-06 14:13:11 -0300

    Add v-for

--- 04-components/src/components/Info.vue
+++ 04-components/src/components/Info.vue
@@ -2,11 +2,25 @@
     <div>
         <p v-if="working">I'm working at the moment.</p>
         <p v-else>Searching for a job.</p>
-        <p>I use the following technologies:</p>
+        <p>I use the following technologies to back-end:</p>
         <ul>
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>Python</li>
+            <li v-for="(technology, index) in backend_technologies" v-bind:key="index">
+                {{ technology }}
+            </li>
+            <!-- 
+                Dessa maneira é possível percorrer a lista que foi declarada em data e exibir um li para cada tecnologia
+                o v-for funciona, mas requisita um id, por isso o index foi declarado como key no v-bind
+            -->
+        </ul>
+        <p>I use the following technologies to front-end:</p>
+        <ul>
+            <li v-for="technology in frontend_technologies" :key="technology.id"> 
+                {{ technology.language }}
+                <!--
+                    Utilizando um objeto a sintaxe fica dessa maneira, já que se quer apenas o dado
+                    e a key já é o proprio id que foi declarado dentro do objeto
+                -->
+            </li>
         </ul>
         <div>
             <button @click="show_email">{{button_text}}</button>
@@ -32,7 +46,13 @@
                 email: 'viniciusmaiamarinho1@gmail.com',
                 meu_link: 'https://google.com',
                 //v-bind faz ligação entre um dado dinâmico e o que está no template
-                button_text: 'Hide email'
+                button_text: 'Hide email',
+                backend_technologies: ['JavaScript', 'TypeScript', 'Python'],
+                frontend_technologies: [
+                    {id:1, language: 'HTML'},
+                    {id:2, language: 'CSS'},
+                    {id:3, language: 'Vue'}
+                ]
             }
         },
         methods: {
