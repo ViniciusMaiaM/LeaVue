84b320bc2c8173bd56b34cc8dfd69ee6c9cc432f
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-17 13:43:27 -0300

    Add get burgers

--- make_your_burger/src/components/Dashboard.vue
+++ make_your_burger/src/components/Dashboard.vue
@@ -2,7 +2,7 @@
     <div id="burger-table">
         <div>
             <div id="burger-table-heading">
-                <div class="order-id">#</div>
+                <div class="order-id">#:</div>
                 <div>Clientes:</div>
                 <div>Pão:</div>
                 <div>Carne:</div>
@@ -11,16 +11,16 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">#</div>
-                <div>Carteira</div>
-                <div>Maminha</div>
-                <div>Cebola</div>
-                <div>Teste</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{burger.nome}}</div>
+                <div>{{burger.pao}}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salada</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
@@ -36,7 +36,27 @@
 
 <script>
     export default{
-        name: "Dashboard"
+        name: "Dashboard",
+        data(){
+            return{
+                burgers: null,
+                burgers_id: null,
+                status: []
+            }
+        },
+        methods:{
+            async getPedidos(){
+                const req = await fetch("http://localhost:3000/burgers");
+
+                const data = await req.json();
+
+                this.burgers = data;
+
+            }
+        },
+        mounted(){
+            this.getPedidos();
+        }
     }
 </script>
 
